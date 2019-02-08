2fdd9b08edd6bb9d36ac6d3c191a710b5dabc452
Pavel Krupets <USAGuerrilla@hotmail.com>
2019-02-07 18:28:46 -0800

    - fixed asembler code to restore all registers changed in the handler

--- software/common/lib-det-os/src/schedule_task_svc.s
+++ software/common/lib-det-os/src/schedule_task_svc.s
@@ -7,17 +7,17 @@
     .fpu vfpv2
     .thumb
 
-    .globl  scheduleTask
-    .section .text.scheduleTask
-    .type   scheduleTask, %function
+    .globl      scheduleTask
+    .section    .text.scheduleTask
+    .type       scheduleTask, %function
 scheduleTask:
     # store R0 and return address on the stack
     # R0 will hold return value from SVC call once SVC call is complete
-    PUSH    {R0, LR}
+    PUSH        {R0, LR}
     # perform SVC call
-    SVC     #1
+    SVC         #1
     # restore registers and return to the caller
-    POP     {R0, PC}
-    .size   scheduleTask, .-scheduleTask
+    POP         {R0, PC}
+    .size       scheduleTask, .-scheduleTask
 
 .end
--- software/common/lib-det-os/src/svc_handler.s
+++ software/common/lib-det-os/src/svc_handler.s
@@ -2,7 +2,6 @@
  * Copyright (C) 2018 Pavel Krupets                                            *
  *******************************************************************************/
 
-# TODO need to do branching once I have more than one system call
 # TODO how to handle parameters which are on stack (for functions which use more than just R0-R4)
 
 .syntax unified
@@ -14,9 +13,11 @@
     .section    .text.SVC_Handler
     .type       SVC_Handler, %function
 SVC_Handler:
-    # store R0 register on the stack as it is used to return result_t from [function]Svc handlers
-    # store return address on the stack
-    PUSH        {R0, LR}
+    # exception handler on ARM stores R3, R2, R1, R0 registers so we don't need to store them
+    # store R4, R5, R6 registers as we are going to change them in our SVC_Handler
+    # store return address
+    # (stack must be 8 bytes aligned so we need to store even number of registers)
+    PUSH        {R4, R5, R6, LR}
     # as per spec ARM®v7-M Architecture Reference Manual
     # B1.5.8 Exception return behavior
     # 2nd bit (starting from zero) is:
@@ -28,16 +29,19 @@ SVC_Handler:
     MRSEQ       R4, MSP
     # load PC value so we can read previous instruction's SVC code
     # (previous instruction is SVS #imm where #imm is 8 bit value)
-    LDR         R5, [R4, #32]
-    # read previous instruction #imm argument value
-    LDRB        R0, [R5, #-2]
+    LDR         R5, [R4, #40]
+    # read previous instruction #imm argument value (into R6)
+    LDRB        R6, [R5, #-2]
+#
+# TODO need to add branching to handle different SVC calls
+#
     # call C function to process SVC OS call
     BL          scheduleTaskSvc
     # store SVC OS call result on the stack so it can be returned from original function
-    STR         R0, [R4, #40]
+    STR         R0, [R4, #48]
     # restore remaining registers we used from the stack
     # to return from exception restore PC value from the stack
-    POP         {R0, PC}
+    POP         {R4, R5, R6, PC}
     .size       SVC_Handler, .-SVC_Handler
 
 .end
