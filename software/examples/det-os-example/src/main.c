b7969279afb75254b0fa6ecbbcc94ecfdb8145c9
Pavel Krupets <USAGuerrilla@hotmail.com>
2019-03-18 21:11:18 -0700

    - added code to measure duty cycle of timer task switching task

--- software/examples/det-os-example/src/main.c
+++ software/examples/det-os-example/src/main.c
@@ -9,6 +9,7 @@
 #include <stm32/stm32l4xx_ll_pwr.h>
 #include <stm32/stm32l4xx_ll_rcc.h>
 #include <stm32/stm32l4xx_ll_tim.h>
+#include <stm32/stm32l4xx_ll_gpio.h>
 #include <stm32/stm32l4xx_ll_utils.h>
 #include <stm32/stm32l4xx_ll_system.h>
 
@@ -46,6 +47,44 @@ static void initializeSystemClock()
     LL_SetSystemCoreClock(80000000);
 }
 
+static void initializeGpio()
+{
+    // PG0 - pin shows duty cycle for timer task (if timer task has code to reset / set pin)
+
+    // reset pin
+    //  LDR R12, =0x48001828
+    //  MOV R0, 0x1
+    //  STR R0, [R12]
+
+    // set pin
+    //  LDR R12, =0x48001818
+    //  MOV R0, 0x1
+    //  STR R0, [R12]
+
+    LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOG);
+
+    if (LL_PWR_IsEnabledVddIO2())
+    {
+        return;
+    }
+
+    LL_PWR_EnablePVM(LL_PWR_PVM_VDDIO2_0_9V);
+
+    while (LL_PWR_IsActiveFlag_PVMO2());
+
+    do
+    {
+        LL_PWR_EnableVddIO2();
+    } while (!LL_PWR_IsEnabledVddIO2());
+
+    LL_PWR_DisablePVM(LL_PWR_PVM_VDDIO2_0_9V);
+
+    LL_GPIO_ResetOutputPin(GPIOG, LL_GPIO_PIN_3 | LL_GPIO_PIN_4 | LL_GPIO_PIN_5 | LL_GPIO_PIN_6 | LL_GPIO_PIN_7 | LL_GPIO_PIN_8 | LL_GPIO_PIN_0 | LL_GPIO_PIN_1);
+    LL_GPIO_SetPinMode(GPIOG, LL_GPIO_PIN_0, LL_GPIO_MODE_OUTPUT);
+    LL_GPIO_SetPinSpeed(GPIOG, LL_GPIO_PIN_0, LL_GPIO_SPEED_FREQ_VERY_HIGH);
+    LL_GPIO_SetOutputPin(GPIOG, LL_GPIO_PIN_0);
+}
+
 static void initializeOsTimer()
 {
     NVIC_SetPriority(TIM2_IRQn, 15);
@@ -54,7 +93,7 @@ static void initializeOsTimer()
     LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_TIM2);
 
     LL_TIM_SetPrescaler(TIM2, __LL_TIM_CALC_PSC(SystemCoreClock, 80000000));
-    LL_TIM_SetAutoReload(TIM2, __LL_TIM_CALC_ARR(SystemCoreClock, LL_TIM_GetPrescaler(TIM2), 10));
+    LL_TIM_SetAutoReload(TIM2, __LL_TIM_CALC_ARR(SystemCoreClock, LL_TIM_GetPrescaler(TIM2), 3000));
 
     LL_TIM_EnableIT_UPDATE(TIM2);
 }
@@ -71,6 +110,7 @@ void initializeTestTimer()
 void initializeHardware()
 {
     initializeSystemClock();
+    initializeGpio();
     initializeOsTimer();
     initializeTestTimer();
 }
