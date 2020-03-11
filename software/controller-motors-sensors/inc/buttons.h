6fb89ab5e96b659e402c81c001d9cb10cbe44c83
Pavel Krupets <usaguerrilla@hotmail.com>
2020-03-10 23:40:55 -0700

    - removed old projects - fixed minor issues in line sensor pcb

--- software/controller-motors-sensors/inc/buttons.h
+++
@@ -1,9 +0,0 @@
-/*******************************************************************************
- * Copyright (C) 2018 Pavel Krupets                                            *
- *******************************************************************************/
-
-#pragma once
-
-void waitForFlushSdButtonPress();
-
-void waitForStartPauseButtonPress();
